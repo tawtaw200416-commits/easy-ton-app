3137a5cf67659a26714ddf18971c15d6d2b585f7
tawtaw200416-commits <tawtaw200416@gmail.com>
2026-04-18 16:28:50 +0630

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,11 +1,8 @@
 const { Telegraf, Markup } = require('telegraf');
-
 const bot = new Telegraf('8732500858:AAFenYSvS3hZ9gB2o0lYYv9fv85KCNWguzk');
 
 bot.start((ctx) => {
-  const message = `💸 You Can Find TON Of Them Easily ✅`;
-  
-  return ctx.reply(message, 
+  return ctx.reply('💸 You Can Find TON Of Them Easily ✅', 
     Markup.inlineKeyboard([
       [Markup.button.webApp('🎮 Open-Easy-TON', 'https://easy-ton-app-iota.vercel.app/')]
     ])
@@ -14,14 +11,9 @@ bot.start((ctx) => {
 
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    try {
-      await bot.handleUpdate(req.body);
-      res.status(200).send('OK');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
-    }
+    await bot.handleUpdate(req.body);
+    res.status(200).send('OK');
   } else {
-    res.status(200).send('Bot is active and ready.');
+    res.status(200).send('Bot is running...');
   }
 };
