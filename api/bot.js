85640ed62a82d6ede02dff50f457efaab638e7d8
tawtaw200416-commits <tawtaw200416@gmail.com>
2026-04-18 14:55:30 +0630

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -3,12 +3,10 @@ const { Telegraf, Markup } = require('telegraf');
 const bot = new Telegraf('8732500858:AAFenYSvS3hZ9gB2o0lYYv9fv85KCNWguzk');
 
 bot.start((ctx) => {
-  // အပေါ်ကစာသားကို Bro လိုချင်တဲ့အတိုင်း ပြောင်းထားပါတယ်
   const message = `💸 You Can Find TON Of Them Easily ✅`;
   
   return ctx.reply(message, 
     Markup.inlineKeyboard([
-      // ခလုတ်စာသားကို Open-Easy-TON လို့ ပြောင်းထားပါတယ်
       [Markup.button.webApp('🎮 Open-Easy-TON', 'https://easy-ton-app-iota.vercel.app/')]
     ])
   );
